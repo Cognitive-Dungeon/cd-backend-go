c2b2151fd00739d809ae80a015367e3845de05d2
Vyacheslav Ivanov <pugemon03@gmail.com>
2025-12-21 02:47:11 +0300

    feat(sharding): add shardid to config creation

--- internal/engine/config.go
+++ internal/engine/config.go
@@ -6,12 +6,14 @@ import "time"
 type Config struct {
 	// Seed - мастер-зерно. От него будут зависеть все уровни.
 	// Level N Seed = MasterSeed + N (или хеш от этого сочетания)
-	Seed int64
+	Seed    int64
+	ShardId uint8
 }
 
 // NewConfig создает конфиг по умолчанию (случайный сид)
 func NewConfig() Config {
 	return Config{
-		Seed: time.Now().UnixNano(),
+		Seed:    time.Now().UnixNano(),
+		ShardId: 0,
 	}
 }
